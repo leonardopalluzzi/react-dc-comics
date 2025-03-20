23a40a2296d9eed7d96a89b63de0940a750808f8
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-20 16:14:35 +0100

    add footer elements

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -2,7 +2,68 @@ export default function Footer() {
 
     return (
         <footer>
-            <h1>Footer component</h1>
+            <section id="top_footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <dl>
+                                <dt>DC COMICS</dt>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+
+                                <dt>DC</dt>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+
+                                <dt>SITES</dt>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+                                <dd></dd>
+
+                                <dt>SHOP</dt>
+                                <dd></dd>
+                                <dd></dd>
+                            </dl>
+                        </div>
+                        <div className="col">
+                            <img src="./img/dc-logo-bg.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="bottom_footer">
+                <div className="container">
+                    <button className="btn"></button>
+                    <div className="social_icons">
+                        <h4>FOLLOW US</h4>
+                        <ul>
+                            <li><a href="">F</a></li>
+                            <li><a href="">T</a></li>
+                            <li><a href="">Y</a></li>
+                            <li><a href="">P</a></li>
+                            <li><a href="">PS</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
         </footer>
     )
 }
