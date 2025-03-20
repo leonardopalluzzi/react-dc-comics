264985b1504f6d1c68f8cc1cdf04e6bab62ae73c
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-20 18:47:25 +0100

    icons section style and fix menu font

--- react-dc-comics/src/components/Icons.jsx
+++ react-dc-comics/src/components/Icons.jsx
@@ -4,11 +4,26 @@ export default function Icons() {
         <section className="icons_section">
             <div className="container">
                 <div className="row">
-                    <div className="col-5">Lorem.</div>
-                    <div className="col-5">Reprehenderit!</div>
-                    <div className="col-5">Non.</div>
-                    <div className="col-5">Porro?</div>
-                    <div className="col-5">Ea.</div>
+                    <div className="col-5">
+                        <img src="./img/buy-comics-digital-comics.png" alt="" />
+                        <span>DIGITAL COMICS</span>
+                    </div>
+                    <div className="col-5">
+                        <img src="./img/buy-comics-merchandise.png" alt="" />
+                        <span>DC MERCHANDISE</span>
+                    </div>
+                    <div className="col-5">
+                        <img src="./img/buy-comics-subscriptions.png" alt="" />
+                        <span>SUBSCRIPTIONS</span>
+                    </div>
+                    <div className="col-5">
+                        <img src="./img/buy-comics-shop-locator.png" alt="" />
+                        <span>SHOP LOCATOR</span>
+                    </div>
+                    <div className="col-5">
+                        <img src="./img/buy-dc-power-visa.svg" alt="" />
+                        <span>DC POWER VISA</span>
+                    </div>
                 </div>
             </div>
         </section>
