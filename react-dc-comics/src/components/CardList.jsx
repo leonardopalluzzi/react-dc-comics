3d536f4489ec4104509c5080aa2b68a18e65de9c
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-21 16:25:00 +0100

    add key id for each card in the list

--- react-dc-comics/src/components/CardList.jsx
+++ react-dc-comics/src/components/CardList.jsx
@@ -7,7 +7,7 @@ export default function CardList() {
             <div className="row">
 
                 {data.map(comic => (
-                    <Card src={comic.thumb} title={comic.series} />
+                    <Card key={comic.id} src={comic.thumb} title={comic.series} />
                 ))}
 
                 {/* <Card src="https://imgs.search.brave.com/aUNyvZBXUulb963JH7KnQm9AMr8bcBoLsiHREOqayIU/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvOWZm/ZWYzMDMtMGZhYy00/OGRkLTg3ODctYzUy/NTk0MDk2ODAwXzEu/MTc1ZDk1Mjg2NzY0/OGEwOTczMTY2NGMy/MTE1NjNlYWIuanBl/Zz9vZG5XaWR0aD02/MTImb2RuSGVpZ2h0/PTYxMiZvZG5CZz1m/ZmZmZmY" title="Action Comics" />
