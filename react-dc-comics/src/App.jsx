4add9ea3922e02a08c4331f35a7bbe6e860702f4
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-21 15:43:49 +0100

    add cards on screen and jumbotron section

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 import Icons from './components/Icons'
+import Jumbotron from './components/Jumbotron'
 
 function App() {
   return (
     <>
       <Header />
+      <Jumbotron />
       <Main />
       <Icons />
       <Footer />
--- react-dc-comics/src/components/Card/Card.jsx
+++ react-dc-comics/src/components/Card/Card.jsx
@@ -4,10 +4,11 @@ export default function Card({ src, title }) { // props destructuring
     console.log(src);
 
     return (
-        <div className="col-2">
+        <div className={style.col_2}>
             <div className={style.card_header}>
                 <img className={style.img_style} src={src} alt={title} />
             </div>
+
             <span className={style.span_style}>{title}</span>
         </div>
     )
