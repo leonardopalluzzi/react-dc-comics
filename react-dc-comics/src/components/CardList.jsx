76412d7e23cd21749702eb9afed14b4c9ef7b24b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-24 18:54:35 +0100

    add all filters

--- react-dc-comics/src/components/CardList.jsx
+++ react-dc-comics/src/components/CardList.jsx
@@ -3,29 +3,84 @@ import data from '../data/dc-comics-2/comics'
 import { useState, useEffect } from "react";
 
 export default function CardList() {
-    const [valori, setValori] = useState(['graphic novel', 'comic book', ""])
-    const [userValue, setUserValue] = useState("")
-    const [filterValue, setFilter] = useState(valori)
+    //series filter
+    const [series, setSeries] = useState("") //user input
 
-    useEffect(() => {
-        setFilter(valori.filter(item => item == userValue))
-    }, [userValue])
+    //artists filter
+    const [artist, setArtist] = useState("") //user input
 
-    const filteredData = data.filter(item =>
-        item.type.includes(filterValue)
-    )
+    //writers filter
+    const [writer, setWriter] = useState("") //user input
+
+    //title filter
+    const [title, setTitle] = useState("") //user input
+
+    //type filter
+    const [type, setType] = useState("")
+
+
+    //filter sum array
+    const [filterValue, setFilter] = useState(type)
+
+    // useEffect(() => {
+    //     setFilter([type, title, writer, artist, series])
+    // }, [type, title, writer, artist, series])
+
+    //console.log(filterValue);
+
+
+
+    const filteredData = data.filter(item => {
+        return (
+            item.type.includes(type)
+            &&
+            item.title.toLowerCase().includes(title)
+            &&
+            item.series.toLowerCase().includes(series.toLowerCase())
+            &&
+            series.toLowerCase().includes(series.toLowerCase())
+            &&
+            item.artists.some(currentArtist => {
+                return (
+                    currentArtist.toLocaleLowerCase().includes(artist.toLocaleLowerCase())
+                )
+            })
+            &&
+            item.writers.some(currentWriters => {
+                return (
+                    currentWriters.toLocaleLowerCase().includes(writer.toLocaleLowerCase())
+                )
+            })
+        )
+    })
+
 
     console.log(filteredData);
 
     return (
         <div>
 
-            <div className="container">
-                <select onChange={(e) => setUserValue(e.target.value)} name="filter" id="filter">
-                    <option value="">select an option</option>
+            <div className="container filters">
+
+                {/* title filter  */}
+                <input placeholder='title' onChange={(e) => setTitle(e.target.value)} value={title} type="text" />
+
+                {/* series filter  */}
+                <input placeholder='series' onChange={(e) => setSeries(e.target.value)} value={series} type="text" />
+
+                {/* artist filter  */}
+                <input placeholder='artist' onChange={(e) => setArtist(e.target.value)} value={artist} type="text" />
+
+                {/* writer filter  */}
+                <input placeholder='writer' onChange={(e) => setWriter(e.target.value)} value={writer} type="text" />
+
+                {/* type filter  */}
+                <select onChange={(e) => setType(e.target.value)} name="filter_name" id="filter">
+                    <option value="">select an type</option>
                     <option value="graphic novel">graphic novel</option>
                     <option value="comic book">comic book</option>
                 </select>
+
             </div>
             <div className="row">
 
